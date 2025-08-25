5fc2622d40002fd036f67de2e050f9693bd9a45f
DDHGDBNHDUEU <mbrodriguez@escuelasproa.edu.ar>
2025-08-25 12:09:08 -0300

    pagina inicial

---
+++ pagina-proa/src/componentes/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <main className="container">
+      <h2>Contenido principal</h2>
+      <p>Aquí puedes agregar tu contenido.</p>
+    </main>
+  );
+}
+
+export default MainContent
---
+++ pagina-proa/src/componentes/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          website_proa
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
