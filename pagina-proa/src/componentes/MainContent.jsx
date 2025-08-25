59e51b118acf5cc9137a945d009aaa01cb74d83a
jojgg <mscolmananrriquez@escuelasproa.edu.ar>
2025-08-25 12:32:10 -0300

    Calendario

--- pagina-proa/src/componentes/MainContent.jsx
+++ pagina-proa/src/componentes/MainContent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Calendario from "./Calendario";
 
 function MainContent() {
   return (
     <main className="container">
       <h2>Contenido principal</h2>
       <p>Aquí puedes agregar tu contenido.</p>
+      <Calendario/>
     </main>
   );
 }
