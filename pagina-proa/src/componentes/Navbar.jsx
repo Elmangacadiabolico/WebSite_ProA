02b86c0de06f5c0a445175b91e72d4f47c557d66
LoreAAG <laagallardo07@gmail.com>
2025-09-01 11:57:22 -0300

    Logo arreglado

--- pagina-proa/src/componentes/Navbar.jsx
+++ pagina-proa/src/componentes/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img
-            src="/docs/5.3/assets/brand/bootstrap-logo.svg"
+            src="https://media.discordapp.net/attachments/1293357057709182986/1412086345018507434/s_0_escuela-proa-logo.png?ex=68b70363&is=68b5b1e3&hm=846997e6ea203f1a0f0e25200d761f0c6bc6f1dfa3fd8139d3076677cd358984&=&format=webp&quality=lossless&width=566&height=569"
             alt="Logo"
             width="30"
             height="24"
