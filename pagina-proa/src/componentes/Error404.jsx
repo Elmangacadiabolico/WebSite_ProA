5e25b24e7d8766b265176573f14220ebf1a1013f
leandro <lriosbas@escuelasproa.edu.ar>
2025-09-08 12:59:31 -0300

    urpagre the page Error404

--- pagina-proa/src/componentes/Error404.jsx
+++ pagina-proa/src/componentes/Error404.jsx
@@ -1,11 +1,23 @@
 import "../CSS/Error404.css"
 function Error404() {
   return (
+    <> 
     <div className="error-page">
-      <h1>404</h1>
-      <p>Oops... La página que buscas no existe o fue movida.</p>
-      <a href="/">Volver al inicio</a>
+    <h1>404</h1>
+  
+    <div className="eyes">
+      <div className="eye"><div className="pupil"></div></div>
+      <div className="eye"><div className="pupil"></div></div>
     </div>
+  
+    <p>Página no encontrada</p>
+    <a href="/">Volver al inicio</a>
+  
+    <span className="falling-text">why?</span>
+    <span className="falling-text">por qué?</span>
+    <span className="falling-text">cómo?</span>
+  </div>
+ </>
   );
 }
 
