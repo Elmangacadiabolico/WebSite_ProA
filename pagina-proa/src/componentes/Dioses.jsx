e2b6f41f7e186a2d57d2dfb2ca37aca3acebf5ed
leandro <lriosbas@escuelasproa.edu.ar>
2025-09-12 12:00:46 -0300

    agreed they acredits

--- pagina-proa/src/componentes/Dioses.jsx
+++ pagina-proa/src/componentes/Dioses.jsx
@@ -5,9 +5,9 @@ const dioses = [
   {
     id: 1,
     name: 'Leandro',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.',
+    description: ' Crador de la pagina, del repositorio, rol de fullstack, Creador de la base de datos, de compartir y de algo de css basico',
     image: 'https://via.placeholder.com/150',
-    github: 'https://github.com',
+    github: 'https://github.com/Elmangacadiabolico',
     linkedin: 'https://linkedin.com',
   },
   {
@@ -20,7 +20,7 @@ const dioses = [
   },
   {
     id: 3,
-    name: 'Dios 3',
+    name: 'Emanuel',
     description: 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.',
     image: 'https://via.placeholder.com/150',
     github: 'https://github.com',
@@ -28,7 +28,7 @@ const dioses = [
   },
   {
     id: 4,
-    name: 'Dios 4',
+    name: 'Lorenzo',
     description: 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio.',
     image: 'https://via.placeholder.com/150',
     github: 'https://github.com',
